004312954d999896ee1889dbec92a7028efdfece
NonSwag <mrminecraft00@gmail.com>
2022-12-26 03:39:02 +0100

    added mouse listener cleaned up imports

--- src/main/java/net/nonswag/autoclicker/api/ui/MainScreen.java
+++ src/main/java/net/nonswag/autoclicker/api/ui/MainScreen.java
@@ -5,11 +5,15 @@ import net.nonswag.autoclicker.api.images.Images;
 import net.nonswag.autoclicker.api.settings.Settings;
 import net.nonswag.core.api.annotation.FieldsAreNonnullByDefault;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @Getter
 @FieldsAreNonnullByDefault
+@ParametersAreNonnullByDefault
 public class MainScreen {
     private JLabel mouse, keyboard, settings;
     private JPanel panel;
@@ -26,20 +30,27 @@ public class MainScreen {
     }
 
     private void initMouse() {
-        init(mouse, Images.MOUSE);
+        init(mouse, Images.MOUSE, () -> System.out.println("mouse"));
     }
 
     private void initKeyboard() {
-        init(keyboard, Settings.getInstance().getTheme().isLight() ? Images.KEYBOARD_LIGHT : Images.KEYBOARD_DARK);
+        Images image = Settings.getInstance().getTheme().isLight() ? Images.KEYBOARD_LIGHT : Images.KEYBOARD_DARK;
+        init(keyboard, image, () -> System.out.println("keyboard"));
     }
 
     private void initSettings() {
-        init(settings, Images.SETTINGS);
+        init(settings, Images.SETTINGS, () -> System.out.println("settings"));
     }
 
-    private void init(JLabel label, Images image) {
+    private void init(JLabel label, Images image, Runnable runnable) {
         label.setBackground(panel.getBackground());
         label.setIcon(image.getIcon());
         label.setOpaque(true);
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent event) {
+                if (MouseEvent.BUTTON1 == event.getButton()) runnable.run();
+            }
+        });
     }
 }
