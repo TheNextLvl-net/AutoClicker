4b34e32d3d38addf77186e104e340a98520473f1
NonSwag <mrminecraft00@gmail.com>
2022-12-27 19:12:07 +0100

    added interval

--- src/main/java/net/nonswag/autoclicker/api/ui/ClickerScreen.java
+++ src/main/java/net/nonswag/autoclicker/api/ui/ClickerScreen.java
@@ -11,21 +11,61 @@ import net.nonswag.core.api.annotation.FieldsAreNonnullByDefault;
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import java.util.concurrent.TimeUnit;
 
 @Getter
 @FieldsAreNonnullByDefault
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class ClickerScreen extends Screen {
-    protected final Clicker clicker;
+    protected JSpinner milliseconds, seconds, minutes;
     protected JLabel power, back, key;
-    protected JPanel panel;
+    protected JPanel panel, interval;
+    protected final Clicker clicker;
     protected boolean locked;
+
+    // Maximum interval of 60 minutes (in milliseconds)
+    private final long maximum = TimeUnit.MINUTES.toMillis(60);
 
     {
         initPanel();
+        initIntervalPanel();
+        initIntervalSpinner();
         initBackButton();
         initPowerButton();
         initKeyButton();
+    }
+
+    private void initIntervalPanel() {
+        interval.setBackground(panel.getBackground());
+    }
+
+    // calculates the interval in milliseconds based on the user input
+    private long getInterval() {
+        return Math.min(maximum, TimeUnit.MINUTES.toMillis(((Number) minutes.getValue()).longValue()) +
+                TimeUnit.SECONDS.toMillis(((Number) seconds.getValue()).longValue()) +
+                ((Number) milliseconds.getValue()).longValue());
+    }
+
+    private void initIntervalSpinner() {
+        initSpinner(milliseconds);
+        initSpinner(seconds);
+        initSpinner(minutes);
+    }
+
+    private void initSpinner(JSpinner spinner) {
+        spinner.addChangeListener(event -> SwingUtilities.invokeLater(() -> {
+            if (!(spinner.getValue() instanceof Number)) return;
+            long interval = getInterval();
+            getClicker().interval(interval);
+            renderTime(interval);
+        }));
+    }
+
+    // renders the time from milliseconds back to the ui
+    private void renderTime(long interval) {
+        minutes.setValue(Math.max(0, Math.min(60, TimeUnit.MILLISECONDS.toMinutes(interval))));
+        seconds.setValue(Math.max(0, TimeUnit.MILLISECONDS.toSeconds(interval) % 60));
+        milliseconds.setValue(Math.max(0, interval % 1000));
     }
 
     private void initBackButton() {
